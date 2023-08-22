bf885ee27eb9780a20a2c920b5a824707a416356
erikbernard <erikbrsilva@gmail.com>
2023-08-21 23:10:10 -0300

    feat: test implementation exchange service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ExchangeModule } from './exchange/exchange.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ExchangeModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -14,6 +14,7 @@ export class ExchangeService {
     try {
       const currencyFrom = await this.currenciesService.getCurrency(from);
       const currencyTo = await this.currenciesService.getCurrency(to);
+      return { amount: (currencyFrom.value / currencyTo.value) * amount };
     } catch (error) {
       throw new Error(error);
     }
