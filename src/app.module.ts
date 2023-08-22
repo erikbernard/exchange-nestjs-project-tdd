5f98cb39925018cedba12e1665322711e3fe83d4
erikbernard <erikbrsilva@gmail.com>
2023-08-22 07:34:51 -0300

    feat: add currencies service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ExchangeModule } from './exchange/exchange.module';
+import { CurrenciesModule } from './currencies/currencies.module';
 
 @Module({
   imports: [ExchangeModule],
