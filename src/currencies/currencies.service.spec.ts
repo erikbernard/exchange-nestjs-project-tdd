5758f8781d00ea23c1ed0f599adc617ff56e910b
erikbernard <erikbrsilva@gmail.com>
2023-08-22 22:06:33 -0300

    feat: test unit getCurrency repository

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CurrenciesService } from './currencies.service';
+import { CurrenciesRepository, CurrenciesService } from './currencies.service';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('CurrenciesService', () => {
   let service: CurrenciesService;
+  let repository: CurrenciesRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrenciesService],
+      providers: [
+        CurrenciesService,
+        {
+          provide: CurrenciesRepository,
+          useFactory: () => ({ getCurrency: jest.fn() }),
+        },
+      ],
     }).compile();
 
     service = module.get<CurrenciesService>(CurrenciesService);
+    repository = module.get<CurrenciesRepository>(CurrenciesRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  describe('getCurrency()', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.getCurrency as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException(),
+      );
+      await expect(service.getCurrency('INVALID')).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+    it('should be not throw if repository returns', async () => {
+      await expect(service.getCurrency('USD')).resolves.not.toThrow();
+    });
+    it('should be called repository with correct params', async () => {
+      await service.getCurrency('USD');
+      expect(repository.getCurrency).toBeCalledWith('USD');
+    });
+    it('should be called repository with correct params', async () => {
+      (repository.getCurrency as jest.Mock).mockReturnValue({
+        currency: 'USD',
+        value: 1,
+      });
+      await service.getCurrency('USD');
+      expect(await service.getCurrency('USD')).toEqual({
+        currency: 'USD',
+        value: 1,
+      });
+    });
   });
 });
--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,6 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
+export class CurrenciesRepository {
+  async getCurrency(currency: string): Promise<any> {}
+}
 @Injectable()
 export class CurrenciesService {
-  async getCurrency(currency: string): Promise<any> {}
+  constructor(private currencyRepository: CurrenciesRepository) {}
+  async getCurrency(currency: string): Promise<any> {
+    try {
+      return await this.currencyRepository.getCurrency(currency);
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
 }
